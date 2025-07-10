e604aefaeb01106d3b398cdcd47aa47763fe9026
David Olagunju <omoekan@yahoo.com>
2025-07-09 23:20:04 -0400

    Update apiConfig.ts

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,3 +1,3 @@
-export const TRANSLATE_API_ENDPOINT = "https://elrah.ngrok.app/translate";
-export const TTS_API_ENDPOINT = "https://room-detection-equipped-settle.trycloudflare.com/pronounce";
-export const BACKEND_BASE_URL = "https://juice-somalia-grain-louis.trycloudflare.com";
+export const TRANSLATE_API_ENDPOINT = ""
+export const TTS_API_ENDPOINT = "";
+export const BACKEND_BASE_URL = "";
